a1929b0a7ed412ca84ce7a32260411333b92b2e9
Matthew Johnson <mattjj@csail.mit.edu>
2013-07-02 14:17:13 -0400

    added fit_hsmm, runs but havent tested correctness

---
+++ real_data_plots/fit_hsmm.py
@@ -0,0 +1,87 @@
+from __future__ import division
+import numpy as np
+import cPickle
+
+import pyhsmm
+import library_models
+from pyhsmm.util.text import progprint_xrange
+
+# NOTE: this hierarchy of mixture models is totall unidentifiable and doesn't
+# predict well because it can't look at temporal cues at all. that also means
+# the hmm and the hsmm don't degrade into this model; they stay better.
+
+num_iter = 200
+training_slice = slice(0,10000)
+
+#############
+#  Loading  #
+#############
+
+### data
+
+f = np.load('/home/mattjj/TMT_50p_5-8-13_processed_notpca.npz')
+data = f['data']
+mus = f['mu']
+sigmas = f['sigma']
+training_data = data[training_slice]
+
+### library
+
+library_size, obs_dim = mus.shape
+
+component_library = \
+        [pyhsmm.basic.distributions.Gaussian(
+            mu=mu,sigma=sigma,
+            mu_0=np.zeros(obs_dim),sigma_0=np.eye(obs_dim), # dummies, not used
+            nu_0=obs_dim+10,kappa_0=1., # more dummies
+            ) for mu,sigma in zip(mus,sigmas)]
+
+#################
+#  Build model  #
+#################
+
+n_states = 200
+
+init_weights = np.random.random((n_states,library_size))
+
+obs_distns = [
+        library_models.FrozenMixtureDistribution(
+            components=component_library,
+            a_0=1.0,b_0=1./5,weights=weights, # for initialization only
+        ) for weights in init_weights]
+
+p_prior, n_prior = 0.5, 100
+alpha_0 = p_prior*n_prior
+beta_0 = (1.0-p_prior)*n_prior
+dur_distns = [pyhsmm.basic.distributions.NegativeBinomialIntegerRVariantDuration(
+    np.ones((10,)),
+    alpha_0=alpha_0,beta_0=beta_0) for state in range(n_states)]
+
+hsmm = library_models.LibraryHSMMIntNegBinVariant(
+        init_state_concentration=10.,
+        alpha_a_0=1.0,alpha_b_0=1./10,
+        gamma_a_0=1,gamma_b_0=1,
+        # alpha=2, gamma=20.0,
+        obs_distns=obs_distns,
+        dur_distns=dur_distns)
+hsmm.add_data(training_data)
+
+##########################
+#  Gather model samples  #
+##########################
+
+for itr in progprint_xrange(num_iter):
+    hsmm.resample_model()
+
+hsmms = []
+for itr in progprint_xrange(1):
+    hsmm.resample_model()
+    hsmms.append(cPickle.loads(cPickle.dumps(hsmm)))
+
+##########
+#  Save  #
+##########
+
+with open('hsmm_results.pickle','w') as outfile:
+    cPickle.dump(hsmms,outfile,protocol=-1)
+
