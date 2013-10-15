12d74f91f48dcf365b9a0b68e097193be0932853
Alex Wiltschko <alex.bw@gmail.com>
2013-10-15 12:43:41 -0400

    Added parallel library subhmm test

---
+++ real_data_plots/parallel-library-subhmms.py
@@ -0,0 +1,79 @@
+from __future__ import division
+import numpy as np
+import cPickle
+
+import pyhsmm
+import pyhsmm.parallel # To kick off the worker imports
+import pyhsmm_library_models.library_subhmm_models as library_subhmm_models
+from pyhsmm.util.text import progprint_xrange
+
+#########################
+#  Prepare the Clients  #
+#########################
+# from IPython.parallel import Client
+# dviews = Client(profile='default')[:]
+# dviews.execute("import pyhsmm_library_models.library_subhmm_models as library_subhmm_models").get() # We slap on a get() to make sure it doesn't error
+
+
+num_iter = 50
+training_slice = slice(0,20000)
+
+#############
+#  Loading  #
+#############
+
+### data
+
+f = np.load('/hms/scratch1/abw11/Data/TMT_6-3-13_median_7x3x3_zscore-norm_madeon_200libsize_8-23-2913-fororchestra.npz')
+
+data = f['data']
+mus = f['means']
+sigmas = f['sigmas']
+training_data = data[training_slice]
+
+### library
+
+library_size, obs_dim = mus.shape
+
+library = \
+        [pyhsmm.basic.distributions.GaussianFixed(
+            mu=mu,sigma=sigma) for mu,sigma in zip(mus,sigmas)]
+
+#################
+#  Build model  #
+#################
+
+Nmaxsuper=20
+
+p_prior, n_prior = 0.5, 100
+alpha_0 = p_prior*n_prior
+beta_0 = (1.0-p_prior)*n_prior
+dur_distns = [pyhsmm.basic.distributions.NegativeBinomialIntegerRVariantDuration(
+    np.r_[0,0,1,1,1,1,1,1,1,1,1,1,1],
+    alpha_0=alpha_0,beta_0=beta_0) for state in range(Nmaxsuper)]
+
+model = library_subhmm_models.HSMMIntNegBinVariantFrozenSubHMMs(
+        alpha_a_0=1.0,alpha_b_0=0.1,
+        gamma_a_0=1,gamma_b_0=1,
+        sub_alpha_a_0=1.,sub_alpha_b_0=1.,sub_gamma_a_0=1.,sub_gamma_b_0=1.,
+        obs_distnss=[library]*Nmaxsuper,
+        dur_distns=dur_distns)
+
+print "Adding parallel training data"
+model.add_data_parallel(training_data)
+
+##########################
+#  Gather model samples  #
+##########################
+
+print "Beginning our resampling"
+for itr in progprint_xrange(num_iter,perline=1):
+    model.resample_model_parallel()
+
+##########
+#  Save  #
+##########
+
+with open('/hms/scratch1/abw11/parallel_frozen_subhmm_results.pickle','w') as outfile:
+    cPickle.dump(model,outfile,protocol=-1)
+
