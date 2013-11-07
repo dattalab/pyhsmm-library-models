df99a3dbce711f6b6ff64564eac95bb4b283bb2a
Matthew Johnson <mattjj@csail.mit.edu>
2013-11-07 14:48:55 -0500

    fixed tempdir

--- library_models.py
+++ library_models.py
@@ -19,7 +19,7 @@ if os.path.exists("/hms/scratch1/"):
     likelihood_cache_dir_hmm = '/hms/scratch1/abw11/tmp/cached_likelihoods_hmm'
 else:
     tempdir = tempfile.gettempdir()
-    likelihood_cache_dir = os.path.join(tempdir, '/cached_likelihoods')
+    likelihood_cache_dir = os.path.join(tempdir, 'cached_likelihoods')
     likelihood_cache_dir_hmm = os.path.join(tempdir, 'cached_likelihoods_hmm')
 
 
